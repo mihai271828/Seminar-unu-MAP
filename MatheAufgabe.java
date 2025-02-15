8209f081dc70d978ff28972fd5a272c98ac18f5d
mihai271828 <pruneanumihai@gmail.com>
2025-02-15 13:11:58 +0200

    added perfekt method

--- MatheAufgabe.java
+++ MatheAufgabe.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MatheAufgabe {
     public double add(double x, double y) {
         return x + y;
@@ -5,7 +9,7 @@ public class MatheAufgabe {
     public double sub(double x, double y) {
         return x - y;
     }
-    public int mul(double x, double y) {
+    public int mul(int x, int y) {
         int result=0;
         for(int i=0; i<=y; i++) {
             result+=x;
@@ -13,5 +17,41 @@ public class MatheAufgabe {
         return result;
     }
 
+    public boolean perfekt(int nr){
+        List<Integer> factors=new ArrayList<>();
+        for(int i=1; i<nr; i++) {
+            if(nr%i==0) {
+                factors.add(i);
+            }
+        }
+        int result=0;
+        for(int i=0; i<factors.size(); i++) {
+            result+=factors.get(i);
+        }
 
+        return nr==result;
+    }
+    public boolean perfekt2(int nr){
+        int count=0;
+        for(int i=1; i<nr; i++) {
+            if(nr%i==0) {
+                count++;
+            }
+        }
+        int index=0;
+        int[] factors=new int[count];
+        for(int i=0; i<nr; i++) {
+            if(nr%i==0) {
+                factors[index] = i;
+                index++;
+            }
+        }
+        int result=0;
+        for(int i=0; i<factors.length; i++) {
+            result+=factors[i];
+        }
+
+        return result==nr;
+
+    }
 }
