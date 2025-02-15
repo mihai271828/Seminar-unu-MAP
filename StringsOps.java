bf702b67b4af35d5331591f8be94d5b04330dce2
mihai271828 <pruneanumihai@gmail.com>
2025-02-15 13:14:52 +0200

    added reverse method

---
+++ StringsOps.java
@@ -0,0 +1,9 @@
+public class StringsOps {
+    public String reverse(String input) {
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+        return reversed;
+    }
+}
