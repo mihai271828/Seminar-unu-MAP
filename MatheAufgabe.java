36f37c8dbe3e1f2173923f737a61c39c77742fff
mihai271828 <pruneanumihai@gmail.com>
2025-02-15 12:18:53 +0200

    Done Mathe Aufagabe class with the main functinalities

---
+++ MatheAufgabe.java
@@ -0,0 +1,17 @@
+public class MatheAufgabe {
+    public double add(double x, double y) {
+        return x + y;
+    }
+    public double sub(double x, double y) {
+        return x - y;
+    }
+    public double mul(double x, double y) {
+        int result=0;
+        for(int i=0; i<=y; i++) {
+            result+=x;
+        }
+        return result;
+    }
+
+
+}
