4570cea6ec416c56dd0c0fce7f9073984a632f1f
mihai271828 <pruneanumihai@gmail.com>
2025-02-15 13:55:55 +0200

    maxDiff method

--- Temperatur.java
+++ Temperatur.java
@@ -5,30 +5,46 @@ public class Temperatur {
     public Temperatur(int[] temperatur) {
         this.temperatur = temperatur;
     }
-    public int average(int[] temperatur) {
+    public int average() {
         int sum = 0;
         for(Integer temperature : temperatur){
             sum += temperature;
         }
         return sum/temperatur.length;
     }
-    public int max(int[] Temperatures){
-        int max = Temperatures[0];
-        for(Integer temperature : Temperatures){
+    public int max(){
+        int max = temperatur[0];
+        for(Integer temperature : temperatur){
             if(temperature > max){
                 max = temperature;
             }
         }
         return max;
     }
-    public int min(int[] Temperatures) {
-        int min = Temperatures[0];
-        for (Integer temperature : Temperatures) {
+    public int min() {
+        int min = temperatur[0];
+        for (Integer temperature : temperatur) {
             if (temperature < min) {
                 min = temperature;
             }
         }
         return min;
+    }
+
+    public int[] maxDifference() {
+        int maxDiff=0;
+        int index=0;
+        for(int i=0; i<temperatur.length-1; i++){
+            if(temperatur[i+1]-temperatur[i]>maxDiff){
+                maxDiff=temperatur[i+1]-temperatur[i];
+                index=i;
+            }
+            if(temperatur[i]-temperatur[i+1]>maxDiff){
+                maxDiff=temperatur[i+1]-temperatur[i];
+                index=i;
+            }
+        }
+        return new int[]{index+1,index+2};
     }
 
 }
